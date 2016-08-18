286b3a651a1b99a925ee4c36202af6b61b14ed47
davidkatz <15Dkatz@shcp.edu>
2016-08-18 08:17:59 -0700

    attempting to add linting

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 // auto-complete codeMirror
+
+// add theme options for the ditor
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
-import Codemirror from 'react-codemirror';
 import AppState from './AppState';
-require('codemirror/mode/javascript/javascript');
+
+import Codemirror from 'react-codemirror';
+import 'codemirror/mode/javascript/javascript';
+
+const CodeMirror = require('codemirror');
 
 @observer
 class App extends Component {
@@ -12,6 +17,20 @@ class App extends Component {
     super(props)
     this.updateCode = this.updateCode.bind(this);
   }
+
+  // componentDidMount() {
+  //   let self = this;
+  //
+  //   this.codemirror = CodeMirror(React.findDOMNode().querySelector('.editor-codemirror'), {
+  //     lineNumbers: true,
+  //     mode: 'javascript',
+  //     lint: function(text, options, cm) {
+  //         let linter = cm.getHelper(CodeMirror.Pos(0, 0), 'lint');
+  //         return linter(text, options);
+  //       },
+  //     gutters: ['CodeMirror-lint-markers']
+  //   })
+  // }
 
   updateCode(code) {
     this.props.appState.updateCode(code);
@@ -25,8 +44,10 @@ class App extends Component {
     let options = {
       lineNumbers: true,
       mode: 'javascript',
+      gutters: ["CodeMirror-lint-markers"],
       tabSize: 2,
-      indentWithTabs: true
+      indentWithTabs: true,
+      lint: true
     };
 
     return (
